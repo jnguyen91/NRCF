11e42c2c0dd224b2caea6e635d25aab033f56074
Lenic <lenic.zhang@gmail.com>
2017-11-17 01:01:45 -0800

    add floating action button to center list activity

--- app/src/main/java/project/community_center/nrcf/MainActivity.java
+++ app/src/main/java/project/community_center/nrcf/MainActivity.java
@@ -1,13 +1,19 @@
 package project.community_center.nrcf;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button map;
+    Button list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +22,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+
+        map = (Button) findViewById(R.id.b_map);
+        list = (Button) findViewById(R.id.b_list);
+
+        map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CenterListActivity.class);
+                startActivity(intent);
             }
         });
     }
+
 
 }
